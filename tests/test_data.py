16556d5c013fa391147c69b48ca7b3c481eaff8a
Amanda Sommer <amanda.m.sommer@hotmail.com>
2023-01-13 14:21:46 +0100

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -19,7 +19,14 @@ def test_data_loading_output_is_tensor():
     assert torch.is_tensor(dataset[0].train_mask), "Train masks are not tensor"
     assert torch.is_tensor(dataset[0].val_mask), "Val masks are not tensor"
     assert torch.is_tensor(dataset[0].test_mask), "Test masks are not tensor"
-    
+ 
+def test_data_size():
+   dataset = x #Data
+   assert len(dataset) == N_train for training and N_test for test
+   assert that each datapoint has shape [1,28,28] or [728] depending on how you choose to format
+   assert that all labels are represented
+
  if __name__ == "__main__":
     test_data_loading_output_is_tensor()
+    test_data_size()
  
