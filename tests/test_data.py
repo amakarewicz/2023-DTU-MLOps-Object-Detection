9277ce62ead74a9b3b8fe6d40fa948fe6dfa7baa
Amanda Sommer <amanda.m.sommer@hotmail.com>
2023-01-13 14:19:50 +0100

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -3,8 +3,7 @@
 import pytest
 import os
 
-_TEST_ROOT = os.path.dirname(__file__)  # root of test folder
-_PROJECT_ROOT = os.path.dirname(_TEST_ROOT)  # root of project
+from tests import _PATH_DATA
 
 
 @pytest.mark.skipif(
