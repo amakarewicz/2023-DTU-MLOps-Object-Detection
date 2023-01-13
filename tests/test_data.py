fa86b12771ff4640cd23e3a437af74b3e52a722d
Amanda Sommer <amanda.m.sommer@hotmail.com>
2023-01-13 12:22:54 +0100

    Update test_data.py

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,1 +1,11 @@
 #TestData
+
+import pytest
+
+@pytest.mark.skipif(
+    not os.path.exists(_PATH_DATA + "/processed")
+    or not os.path.exists(_PROJECT_ROOT + "/config"),
+    reason="Data and config files not found",
+)
+def test_function():
+    ...
