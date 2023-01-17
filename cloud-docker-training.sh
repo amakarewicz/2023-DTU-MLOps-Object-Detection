9845c03ce95d4e725a2bd2ad8be1478c73928d29
Agata Makarewicz <43831153+amakarewicz@users.noreply.github.com>
2023-01-17 15:30:27 +0100

    change to python 3.8

--- cloud-docker-training.sh
+++ cloud-docker-training.sh
@@ -2,5 +2,5 @@
 # exit when any command fails
 set -e
 dvc pull
-python3.9 -u src/models/train_model.py
+python3.8 -u src/models/train_model.py
 # gsutil -m cp -r outputs gs://dtu-mlops-project-training-output
